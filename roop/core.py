ac5145282b9dd80defb0a20f8c636e0942f204bf
ssdan46 <160417040+ssdan46@users.noreply.github.com>
2024-02-19 01:47:37 +0800

    Update core.py

--- roop/core.py
+++ roop/core.py
@@ -87,7 +87,7 @@ def suggest_execution_providers() -> List[str]:
 
 def suggest_execution_threads() -> int:
     if 'CUDAExecutionProvider' in onnxruntime.get_available_providers():
-        return 16
+        return 8
     return 1
 
 
